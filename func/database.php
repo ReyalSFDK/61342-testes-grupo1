377d8ea2433d4858595f9042ebeda94e5a1033eb
reyalsFDK <luan.oliveira.7587@gmail.com>
2019-12-07 11:41:13 -0300

    added get list content and database connection

--- func/database.php
+++ func/database.php
@@ -1,23 +1,12 @@
 <?php
 
-/**
- * Retorna um cabeçalho HTML
- *
- * @param string $title
- * @return string
- */
-function setHeader(string $title) {
-    return '
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <!-- Required meta tags -->
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            
-                <!-- Bootstrap CSS -->
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            
-                <title>'. $title.'</title>
-            </head>';
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "ucolec";
+
+$conn  = new MySQLi($host, $user, $pass, $database );
+
+if($conn->connect_error){
+    echo "Desconectado! Erro: " . $conn->connect_error;
 }
